a2f27684c32824d8f6ef8f5b6fa786f7c8b3d7e3
mickeyminion14 <sarthakagrawal6@gmail.com>
2019-03-15 10:06:57 +0530

    second commit

---
+++ build/precache-manifest.3dc6cd9aa3303c6dafc9e8626b8ba9b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28ec845f7cd01f6b5735",
+    "url": "/static/css/main.fe42f037.chunk.css"
+  },
+  {
+    "revision": "28ec845f7cd01f6b5735",
+    "url": "/static/js/main.ee9cd1b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47c4dfd380322a61408a",
+    "url": "/static/css/2.4de7b681.chunk.css"
+  },
+  {
+    "revision": "47c4dfd380322a61408a",
+    "url": "/static/js/2.127620e9.chunk.js"
+  },
+  {
+    "revision": "97e58094cef5392adcdd38db671083be",
+    "url": "/index.html"
+  }
+];
